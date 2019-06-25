88ad1966c7135194ea29ddcde2b592c127873641
lisachu98 <maksyminator@gmail.com>
2019-06-25 10:55:54 +0200

    Dodanie bloczków

--- TPP4/Form1.cs
+++ TPP4/Form1.cs
@@ -12,9 +12,33 @@ namespace TPP4
 {
     public partial class Form1 : Form
     {
+        public struct Bloczek
+        {
+            public int x;
+            public int y;
+            public int waga;
+
+        }
+        List<Bloczek> Bloki = new List<Bloczek>();
         public Form1()
         {
             InitializeComponent();
+            int rozmiarBloczka = 50;
+            int[] wagi = { 10, 50, 20, 30, 100, 15, 40, 40, 10};
+            for(int i = 0; i < 9; i++)
+            {
+                Bloczek bloczek = new Bloczek();
+                bloczek.x = 70 + i * 50 + i * 10;
+                bloczek.y = panel1.Height - rozmiarBloczka;
+                bloczek.waga = wagi[i];
+                Bloki.Add(bloczek);
+            }
+            panel1.Refresh();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
         }
     }
 }
