38b491e559986f0dd01e02fd5e0e2ca2eea23b16
lisachu98 <maksyminator@gmail.com>
2019-06-25 12:35:57 +0200

    Kolizje + test ruchu w prawo

--- TPP4/Form1.Designer.cs
+++ TPP4/Form1.Designer.cs
@@ -35,6 +35,8 @@
             this.button4 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // panel1
@@ -48,16 +50,17 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(153, 632);
+            this.button1.Location = new System.Drawing.Point(93, 632);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 20);
             this.button1.TabIndex = 1;
             this.button1.Text = "Gora";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(153, 661);
+            this.button2.Location = new System.Drawing.Point(93, 661);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 20);
             this.button2.TabIndex = 2;
@@ -66,16 +69,17 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(234, 661);
+            this.button3.Location = new System.Drawing.Point(174, 661);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 20);
             this.button3.TabIndex = 3;
             this.button3.Text = "Prawo";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(72, 661);
+            this.button4.Location = new System.Drawing.Point(12, 661);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(75, 20);
             this.button4.TabIndex = 4;
@@ -85,7 +89,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(420, 661);
+            this.textBox1.Location = new System.Drawing.Point(512, 661);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 0;
@@ -93,17 +97,39 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(417, 639);
+            this.label1.Location = new System.Drawing.Point(509, 639);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(95, 13);
             this.label1.TabIndex = 5;
             this.label1.Text = "Maksymalna waga";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(332, 658);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Chwyc";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(320, 615);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(10, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = " ";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(625, 713);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button4);
@@ -128,6 +154,8 @@
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- TPP4/Form1.cs
+++ TPP4/Form1.cs
@@ -23,10 +23,11 @@ namespace TPP4
             public int x;
             public int y;
             public int maxWaga;
+            public int idBloczka;
         }
         int dzwigWysokosc = 450;
         int dzwigSzerokosc = 10;
-        int dzwigDlugosc = 500;
+        int dzwigDlugosc = 450;
         int bloczekRozmiar = 40;
         int hakRozmiar = 6;
         List<Bloczek> bloki = new List<Bloczek>();
@@ -37,6 +38,10 @@ namespace TPP4
             
             hak.x = 72;
             hak.y = 210;
+            hak.idBloczka = -1;
+            hak.maxWaga = 30;
+            textBox1.Text = hak.maxWaga.ToString();
+            
             int[] wagi = { 10, 50, 20, 30, 100, 15, 40, 40, 10};
             for(int i = 0; i < 9; i++)
             {
@@ -54,7 +59,7 @@ namespace TPP4
             System.Drawing.SolidBrush pen = new System.Drawing.SolidBrush(System.Drawing.Color.Orange);
             Graphics g = panel1.CreateGraphics();
             g.FillRectangle(pen, new Rectangle(panel1.Width / 5 - dzwigSzerokosc - 50, panel1.Height - dzwigWysokosc, dzwigSzerokosc, dzwigWysokosc));
-            g.FillRectangle(pen, new Rectangle(panel1.Width / 5 - dzwigSzerokosc - 100, panel1.Height - dzwigWysokosc + 50, dzwigDlugosc, dzwigSzerokosc));
+            g.FillRectangle(pen, new Rectangle(panel1.Width / 5 - dzwigSzerokosc - 50, panel1.Height - dzwigWysokosc + 50, dzwigDlugosc, dzwigSzerokosc));
             g.FillRectangle(pen, new Rectangle(hak.x + 4, panel1.Height - dzwigWysokosc + 50 + dzwigSzerokosc, 2, hak.y - (panel1.Height - dzwigWysokosc + 50 + dzwigSzerokosc)));
             pen = new System.Drawing.SolidBrush(System.Drawing.Color.Black);
             g.FillRectangle(pen, new Rectangle(hak.x-2, panel1.Height - dzwigWysokosc + 50, dzwigSzerokosc, dzwigSzerokosc));
@@ -62,6 +67,29 @@ namespace TPP4
             g.FillRectangle(pen, new Rectangle(hak.x, hak.y, hakRozmiar, hakRozmiar));
             pen = new System.Drawing.SolidBrush(System.Drawing.Color.Red);
             for (int i = 0; i < bloki.Count; i++) g.FillRectangle(pen, new Rectangle(bloki[i].x, bloki[i].y, bloczekRozmiar, bloczekRozmiar));
+            pen.Dispose();
+            g.Dispose();
+        }
+
+        private int kolizjeHakBlok()
+        {
+            label2.Text = " ";
+            for(int i = 0; i < bloki.Count; i++)
+            {
+                if (hak.x + hakRozmiar >= bloki[i].x && hak.x <= bloki[i].x + bloczekRozmiar && hak.y + hakRozmiar >= bloki[i].y && hak.y <= bloki[i].y + bloczekRozmiar) return i;
+            }
+            return -1;
+        }
+
+        private int kolizjeBlokBlok(Bloczek bloczek)
+        {
+            label2.Text = " ";
+            for (int i = 0; i < bloki.Count; i++)
+            {
+                if (hak.idBloczka == i) i++;
+                if (i != bloki.Count && bloczek.x + bloczekRozmiar > bloki[i].x && bloczek.x < bloki[i].x + bloczekRozmiar && bloczek.y + bloczekRozmiar > bloki[i].y && bloczek.y < bloki[i].y + bloczekRozmiar) return i;
+            }
+            return -1;
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -72,6 +100,18 @@ namespace TPP4
         private void button4_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            hak.x += 10;
+            panel1.Refresh();
         }
     }
 }
